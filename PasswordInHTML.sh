6ca9b591bf6fdbaa556f06b3b58627543d3f00b6
FlowHack <flow.hack.com@gmail.com>
2021-10-10 17:41:21 +0000

    внесены изменения связанные с обновлением программы

--- PasswordInHTML.sh
+++ PasswordInHTML.sh
@@ -14,24 +14,19 @@ else
 	sudo apt install xclip
 
 	echo "!!!!!Установка python3-venv!!!!!"
-	sleep 2
 	sudo apt install python3-venv -y
 
 	echo "!!!!!Установка python3-tk=3.8.5-1~20.04.1!!!!!"
-	sleep 2
 	sudo apt install python3-tk
 
 
 	echo "!!!!!Установка python3-pip!!!!!"
-	sleep 2
 	sudo apt install python3-pip -y
 
 	echo "!!!!!Создание виртуального окружения!!!!!"
-	sleep 2
 	python3 -m venv venv
 	source "$PWD/venv/bin/activate"
 	echo "!!!!!Установка зависимостей!!!!!"
-	sleep 2
 	pip3 install wheel
 	pip3 install -r requirements.txt
 
