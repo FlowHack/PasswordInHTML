7f47130da720e21556e54894dfef75c414c51b25
flowhack <flow.hack.com@gmail.com>
2021-11-04 20:51:36 +0300

    исправление словаря шифратора

--- settings/encryption.py
+++ settings/encryption.py
@@ -3,7 +3,7 @@ from os.path import isfile
 from run import get_dict_from_json_file, write_dict_in_file
 from settings import path_to_passwords_settings
 
-ENCRYPTION_ALPHABET = '''Й Ц У К Е Н Г Ш Щ З Х Ъ Ё Ф Ы В А П Р О Л Д Ж Э Я Ч С М И Т Ь Б Ю й ц у к е н г ш щ з х ъ ё ф ы в а п р о л д ж э я ч с м и т ь б ю Q W E R T Y U I O P A S D F G H J K L Z X C V B N M q w e r t y u i o p a s d f g h j k l z x c v b n m 0 1 2 3 4 5 6 7 8 9 ^ @ ! 1 " # $ % & ' ( ) * : + ; { , < L \ | - = } . > ~ / ? — _ [ ]'''
+ENCRYPTION_ALPHABET = '''Й Ц У К Е Н Г Ш Щ З Х Ъ Ё Ф Ы В А П Р О Л Д Ж Э Я Ч С М И Т Ь Б Ю й ц у к е н г ш щ з х ъ ё ф ы в а п р о л д ж э я ч с м и т ь б ю Q W E R T Y U I O P A S D F G H J K L Z X C V B N M q w e r t y u i o p a s d f g h j k l z x c v b n m 0 1 2 3 4 5 6 7 8 9 ^ @ ! 1 " # $ % & ' ( ) * : + ; { , < \ | - = } . > ~ / ? — _ [ ]'''
 
 class Encryption:
     def __init__(self):
--- settings/variables.py
+++ settings/variables.py
@@ -1,7 +1,7 @@
 from os import getcwd
 from os.path import join as path_join
 
-VERSION = '1.0.4'
+VERSION = '1.0.5'
 
 UPDATE_WIN = 'updater.exe'
 UPDATE_LINUX = 'updater.sh'
